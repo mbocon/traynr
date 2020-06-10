979adcf6bbc63efd7d3ff64354bd449927439690
Michael Bocon <mikebocon@gmail.com>
2020-06-09 20:35:55 -1000

    new project

--- src/index.js
+++ src/index.js
@@ -1,17 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "../src/components/App";
+import { createBrowserHistory } from 'history';
+import { Switch, Route, Router } from 'react-router';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const routes = [
+    {
+        path: '/',
+        component: App,
+        name: 'Main-Page'
+    }
+]
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const root = document.getElementById("root");
+const hist = createBrowserHistory();
+
+ReactDOM.render(<Router history={hist}>
+        <Switch>
+            {
+                routes.map((route)=>{
+                    return(
+                        <Route path={route.path} component={route.component} key={route.name}></Route>
+                    )
+                })
+            }
+        </Switch>
+    </Router>, root);
+
